5bb1cdb2a1791da1496de57299a45f1b060ea920
Andrew Swearengen <andrew.swearengen@gmail.com>
2016-10-13 17:10:57 -0700

    new pages, routes, and menu

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
-
+  root 'pages#home'
+  get 'about' => 'pages#about'
+  get 'contact' => 'pages#contactus'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
