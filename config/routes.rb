346450905e6562a3440076d5577db5955dd9abeb
Andrew Swearengen <andrew.swearengen@gmail.com>
2016-10-23 21:41:05 -0700

    added devise, customized devise routes, added custom css for devise authentication pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_for :users
+  as :user do
+  	get 'login' => 'devise/sessions#new'
+  	delete 'logout' => 'devise/sessions#destroy'
+  	get 'signup' => 'devise/registrations#new'
+  end
   root 'pages#home'
   get 'about' => 'pages#about'
   get 'contact' => 'pages#contactus'
